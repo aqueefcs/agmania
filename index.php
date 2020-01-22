350d80ec9651c215c8b69bd99df8c788df04287d
aqueefcs <53555185+aqueefcs@users.noreply.github.com>
2020-01-23 00:45:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -25,8 +25,9 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="google-site-verification" content="1Z_rMs1cZL6Z0jn1rR32UWekFB_EXpqhM59FRTpZZ4s" />
 	<title>AGMANIA 2020</title>
 	<!-- Font Awesome -->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
