91a8e2e545b75e2490f379378e3fb183be372f63
aqueefcs <53555185+aqueefcs@users.noreply.github.com>
2020-01-22 23:24:27 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 
 		//this code is to connect with db
 		$timezone = date_default_timezone_set("Asia/kolkata");
-		$con = mysqli_connect("remotemysql.com","MASntFxpxi", "eY8wvb0kqF","agmania");
+		$con = mysqli_connect("remotemysql.com","MASntFxpxi", "eY8wvb0kqF","MASntFxpxi");
 
 		if(mysqli_connect_errno()){
 			echo "Failed to Connect ".mysqli_connect_errno();
