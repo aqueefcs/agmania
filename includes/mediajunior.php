e5d0a32dc27943fdbb2ebbb0689fdc45d41cd454
aqueefcs <53555185+aqueefcs@users.noreply.github.com>
2020-01-23 20:42:55 +0530

    Update mediajunior.php

--- includes/mediajunior.php
+++ includes/mediajunior.php
@@ -666,6 +666,7 @@
     function rotateCard(btn){
         var $card = $(btn).closest('.card-container');
         console.log($card);
+	clearTimeout(timerId);
         if($card.hasClass('hover')){
             $card.removeClass('hover');
         } else {
