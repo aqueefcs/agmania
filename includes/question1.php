12d3758fbdc06545082807dc205c54bd12996309
aqueefcs <53555185+aqueefcs@users.noreply.github.com>
2020-01-24 20:07:30 +0530

    Update question1.php

--- includes/question1.php
+++ includes/question1.php
@@ -239,6 +239,8 @@
         } else {
             $card.addClass('hover');
 	    clearTimeout(timerId);
+	    var audioans = new Audio('../assets/music/short_sms.mp3');
+            audioans.play();
         }
     }
 
