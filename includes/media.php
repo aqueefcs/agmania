c9f00d416dc6542552f7622fb999f15b332e5629
aqueefcs <53555185+aqueefcs@users.noreply.github.com>
2020-01-24 20:05:41 +0530

    Update media.php

--- includes/media.php
+++ includes/media.php
@@ -670,6 +670,8 @@
         } else {
             $card.addClass('hover');
 		clearTimeout(timerId);
+		var audioans = new Audio('../assets/music/short_sms.mp3');
+            	audioans.play();
         }
     }
 
