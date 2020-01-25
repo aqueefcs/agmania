e79c1de0d48ce804fd375d4db20f5a2d31d0e7f8
aqueefcs <53555185+aqueefcs@users.noreply.github.com>
2020-01-25 18:09:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -398,7 +398,7 @@ body {
       <div class="card">
 
         <!-- Card image -->
-        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap">
+        <img class="card-img-top" src="assets/img/hm.jpg" alt="Card image cap">
 
         <!-- Card content -->
         <div class="card-body">
