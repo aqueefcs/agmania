9d962056990285ee15915996cdf44ce6e1761931
aqueefcs <53555185+aqueefcs@users.noreply.github.com>
2020-01-26 12:29:07 +0530

    Update mediajunior.php

--- includes/mediajunior.php
+++ includes/mediajunior.php
@@ -748,7 +748,7 @@
 		    			document.getElementById("ques").style.marginBottom = "100px";
 		    			
 		    			
-			    		var timeLeft = 30;
+			    		var timeLeft = 20;
 						var elem = document.getElementById('timer');
 						var timerId = setInterval(countdown, 1000);
 						var audio = new Audio('../assets/music/beep.mp3');
